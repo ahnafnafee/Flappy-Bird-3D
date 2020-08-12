4286a2cbdd627574e93fc04b2daea5ffc648a713
Ahnaf An Nafee <nafee01@gmail.com>
2020-08-12 16:37:49 -0400

    Added temp shader

--- Assets/Scripts/FlappyControl.cs
+++ Assets/Scripts/FlappyControl.cs
@@ -10,11 +10,16 @@ public class FlappyControl : MonoBehaviour
     // Implement gravity
     public float gravity = 1.0f;
     public float flapSpeed = 5.0f;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    
+    private Rigidbody _rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,12 +29,23 @@ public class FlappyControl : MonoBehaviour
         transformVar.position = transformVar.position + Vector3.down * (Time.deltaTime * speed);
         // v = u + at
         speed = speed + gravity * Time.deltaTime;
-
+    
         if (Input.GetKeyDown(KeyCode.Space))
         {
             speed = -flapSpeed;
         }
     }
+
+    // private void FixedUpdate()
+    // {
+    //     if (_rb.velocity.z < 0)
+    //     {
+    //         _rb.velocity += Vector3.up * (Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
+    //     } else if (_rb.velocity.z > 0 && !Input.GetButton("Jump"))
+    //     {
+    //         _rb.velocity += Vector3.up * (Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime);
+    //     }
+    // }
 
     public void OnCollisionEnter(Collision other)
     {
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Range(1, 10)] public float jumpVelocity;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            _rigidbody.velocity = Vector3.up * jumpVelocity;
+        }
+    }
+}
